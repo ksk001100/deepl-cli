80a3a066cd023ee5ae8b43b032c54049c127ba75
Keisuke Toyota <hm.pudding0715@gmail.com>
2022-12-06 20:01:28 +0900

    fix

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # DeepL CLI
+
+## Install
+
+```bash
+$ git clone https://github.com/ksk001100/deepl-cli
+$ cd deepl-cli
+$ cargo install --path .
+```
+or
+
+Download [here](https://github.com/ksk001100/deepl-cli/releases)
+
+## Usage
+
+```bash
+$ export DEEPL_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+$ deepl --help # => Show help
+$ deepl こんにちは世界 # => Default target language (EN)
+$ deepl Hello world --language EN
+```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use async_std::task;
 use seahorse::{App, Context, Flag, FlagType};
 use serde::Deserialize;
 use std::env;
 use std::process::exit;
-use surf::http::Url;
+use reqwest::{self, Url};
+use tokio::runtime::Runtime;
 
 #[derive(Deserialize, Debug)]
 struct Translation {
+    #[allow(dead_code)]
     detected_source_language: String,
     text: String,
 }
@@ -16,26 +17,13 @@ struct Response {
     translations: Vec<Translation>,
 }
 
-async fn translate<'a>(auth_key: &'a str, text: &'a str, target_lang: &'a str) -> surf::Result<()> {
+async fn translate<'a>(auth_key: &'a str, text: &'a str, target_lang: &'a str) -> Result<(), Box<dyn std::error::Error>> {
     let url = Url::parse(&format!(
         "https://api-free.deepl.com/v2/translate?auth_key={}&text={}&target_lang={}",
         auth_key, text, target_lang
     ))?;
 
-    let res: Response = match surf::get(url).recv_string().await {
-        Ok(res) => match serde_json::from_str(&res) {
-            Ok(res) => res,
-            Err(e) => {
-                eprintln!("{}", e);
-                exit(1);
-            }
-        },
-        Err(e) => {
-            eprintln!("{}", e);
-            exit(1);
-        }
-    };
-
+    let res: Response = reqwest::get(url).await?.json().await?;
     for translation in res.translations {
         println!("{}", translation.text)
     }
@@ -44,7 +32,8 @@ async fn translate<'a>(auth_key: &'a str, text: &'a str, target_lang: &'a str) -
 }
 
 fn action(c: &Context) {
-    task::block_on(async {
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
         let auth_key = match c.string_flag("key") {
             Ok(key) => key,
             Err(_) => match env::var("DEEPL_KEY") {
